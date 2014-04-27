ae99fa85272ab298737d2f4ce0daf9be3dae7b63
Dazza <mcadam@gmail.com>
2014-04-27 15:33:35 +1000

    Homepage

---
+++ index.php
@@ -0,0 +1,115 @@
+<?php include("_head.php"); ?>
+
+</head>
+
+<body class="home">
+
+<div id="page">
+	<a id="open-icon-header" href="#menu"></a>
+
+
+	<section role="homepage" class="homepage">
+
+		<header>
+		    <div class="row">
+		        <div class="small-4 small-offset-8 columns text-right auth">
+		            <a href="/player/"><img width="30" height="30" src="http://www.gravatar.com/avatar/b96be315140d8ca5f50b03267673c155?s=30&amp;d=wavatar" title="2 Jack Dummy" alt="2 Jack Dummy" /></a>
+		            <a href="/auth/logout">log-out</a>
+		        </div><!-- /large-2 --> 
+		    </div><!-- /row -->
+		</header><!-- /utilityBar -->
+
+		<div class="row">
+			<div class="large-12 columns">
+				<h1>Play tennis? Play Tennis Comp in Sydney!</h1>
+				<h4>Tennis Comp is a singles tennis competition played over 3 sets. All standards are welcome. A great way to meet new tennis players and have fun improving your game</h4>
+				<p>Play 9 tennis matches in each round including a semi and grand final. Top two go up a division. Bottom two go down a division. Divisions are mixed. A great way to meet new tennis players and improve your game. Sign up and give it a go.</p>
+				<p>There are 4 rounds each year lasting for 9 weeks. Early registration is recommended to ensure a spot.</p>
+			</div><!-- /columns -->
+		</div><!-- row -->
+    
+		<div class="row">
+			<div class="large-12 columns">
+				<div class="row">
+					<div class="large-5 columns">
+						<h3 class="text-left">The active round <strong>Summer Comp</strong> ends in <span class="flip"><span class="stack"></span>1</span> days</h3>
+						<a href="http://www.tenniscomp.com.au/leagues/" class="button ">See Comp Results</a>
+					</div>
+					<div class="large-5 columns">
+						<h3 class="text-left">The next Round <strong>Autumn Comp</strong> starts in <span class="flip"><span class="stack"></span>1</span> days</h3>
+						<a href="http://www.tenniscomp.com.au/player/my-league/select-league" class="button">Sign-up Autumn Comp</a>
+					</div>
+				</div>
+			</div>
+		</div><!-- row -->
+
+		<div class="row">
+			<div class="large-3 columns small-centered">
+				<a href="http://www.tenniscomp.com.au/#" class="button">Scroll Down</a>
+			</div>
+		</div><!-- row -->
+	</section><!-- homepage -->
+
+
+
+<section role="features" class="features">
+
+     <div class="row">
+        <div class="large-12 columns">
+            <h4>An easy way to meet new tennis players, improve your game and play fun competitive singles tennis over 3 sets</h4>
+        </div>
+     </div>
+
+     <div class="row">
+        <div class="large-4 columns">
+            <h4>Where can you play?</h4>            
+            <p>Tennis Comp started in Sydney and is growing if there isn't a comp near you get in touch and let us know, we only need 8 players to start a comp in your area.</p>
+            <p>You can see the current active comps that you can join here:</p>
+         </div><!-- four -->
+        <div class="large-4 columns">
+            <h4>Rules is rules!</h4>
+            <p>Players must follow the rules. And there is a 3 strikes rule to ensure no dick heads.</p>
+            <p>Matches follow the standard WTA tennis rules of match play, plus a few additional Tennis Comp rules.</p>
+        </div><!-- four -->
+        <div class="large-4 columns">
+            <h4>Ready to play? Sign-up!</h4>
+            <p>Anyone over 18 can play. All standards are welcome. You will be graded by ability when you sign-up and placed in the right division.</p>
+            <p>The first step is to sign-up and create an account. And then join a Comp.</p>
+        </div><!-- four -->
+    </div><!-- row -->
+
+    <div class="row">
+        <div class="large-4 columns" style="xbackground-color:white;">
+             <a href="http://www.tenniscomp.com.au/leagues/" class="button pink mini fullwidth">Find Active Comps</a>
+             <a href="http://www.tenniscomp.com.au/leagues/" class="button navy fill">Find Active Comps</a>
+             <a href="http://www.tenniscomp.com.au/leagues/" class="button green">Find Active Comps</a>
+        </div><!-- four -->
+        <div class="large-4 columns">
+             <a href="http://www.tenniscomp.com.au/how-to-play/rules" class="button navy fresh fill">Tennis Comp Rules</a>
+        </div><!-- four -->
+        <div class="large-4 columns">
+             <!-- <a href="http://www.tenniscomp.com.au/auth/register" class="button dark">Sign-up</a> -->
+        </div><!-- four -->
+
+    
+    </div><!-- row -->
+
+<?php include("_footer.php"); ?>
+
+</section><!-- reveal -->
+
+
+</div><!-- fluid-column -->
+
+<?php include("_sidebar.php"); ?>
+
+</body>
+</html>
+
+
+
+ 
+
+
+
+
